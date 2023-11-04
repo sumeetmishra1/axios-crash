5ef4cec0b4c8e10d82a937d97ea868b7b78eb7b2
sumeetmishra1 <sumitsfs0@gmail.com>
2023-11-04 12:45:48 +0530

    axios

--- main.js
+++ main.js
@@ -1,52 +1,136 @@
+//AXIOS GLOBALS
+axios.defaults.headers.common['X-Auth_Token'] = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
 // GET REQUEST
 function getTodos() {
-  console.log('GET Request');
+  // axios({
+  //   method: 'get',
+  //   url:'https://jsonplaceholder.typicode.com/todos'
+  // })
+  // .then(res=>showOutput(res))
+  // .catch(err=>console.log(err));
+  axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5',{timeout : 1000})
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // POST REQUEST
 function addTodo() {
-  console.log('POST Request');
+  axios.post('https://jsonplaceholder.typicode.com/todos',{
+    title: 'new todo',
+    completed:false
+  })
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // PUT/PATCH REQUEST
 function updateTodo() {
-  console.log('PUT/PATCH Request');
+  axios.patch('https://jsonplaceholder.typicode.com/todos/1',{
+    title: 'new updated todo',
+    completed: true
+  })
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // DELETE REQUEST
 function removeTodo() {
-  console.log('DELETE Request');
+  axios.delete('https://jsonplaceholder.typicode.com/todos/1')
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // SIMULTANEOUS DATA
 function getData() {
-  console.log('Simultaneous Request');
+  axios.all([
+    axios.get('https://jsonplaceholder.typicode.com/todos'),
+    axios.get('https://jsonplaceholder.typicode.com/posts?_limit=5')
+  ])
+  .then(axios.spread((todos,posts )=> showOutput(posts)))
+  .catch(err=>console.log(err));
 }
 
 // CUSTOM HEADERS
 function customHeaders() {
-  console.log('Custom Headers');
+  const config ={
+    headers:{
+      'Content':'application/json',
+      Authorization:'sometoken'
+    }
+  }
+  axios.post('https://jsonplaceholder.typicode.com/todos',{
+    title: 'new todo',
+    completed:false
+  },config)
+  .then(res=>showOutput(res))
+  .catch(err=>console.log(err));
 }
 
 // TRANSFORMING REQUESTS & RESPONSES
 function transformResponse() {
-  console.log('Transform Response');
+  const options ={
+    method:'post',
+    url:'https://jsonplaceholder.typicode.com/todos',
+    data:{
+      title:"Hello world"
+    },
+    transformResponse: axios.defaults.transformResponse.concat(data=>{
+      data.title = data.title.toUpperCase();
+      return data;
+    })
+  }
+
+  axios(options).then(res => showOutput(res))
 }
 
 // ERROR HANDLING
 function errorHandling() {
-  console.log('Error Handling');
+  axios.get('https://jsonplaceholder.typicode.com/todoss')
+  .then(res=>showOutput(res))
+  .catch(err=>{
+    if(err.response){
+      console.log(err.response.data);
+      console.log(err.response.status);
+      console.log(err.response.headers);
+      if(err.response.status===404){
+        alert('Error: Page Not Found');
+      }else{
+        console.error(err.message);
+      }
+    }
+  });
 }
 
 // CANCEL TOKEN
 function cancelToken() {
-  console.log('Cancel Token');
+  const source=axios.CancelToken.source();
+  axios.get('https://jsonplaceholder.typicode.com/todos',{
+    cancelToken :source.token
+  })
+  .then(res=>showOutput(res))
+  .catch(thrown=>{
+    if(axios.isCancel(thrown)){
+      console.log('Request Canceled',thrown.message);
+    }
+  });
+  if(true){
+    source.cancel('Rewuest canceled');
+  }
 }
 
 // INTERCEPTING REQUESTS & RESPONSES
-
+axios.interceptors.request.use(config=>{
+  console.log(`${config.method.toUpperCase()} request send to ${config.url} at ${new Date().getTime()}`);
+  return config
+},error=> {
+  return Promise.reject(error);
+});
 // AXIOS INSTANCES
-
+const axiosInstance=axios.create({
+  baseURL:'https://jsonplaceholder.typicode.com'
+})
+//axiosInstance.get('/comments?_limit=5').then(res=> showOutput(res));
 // Show output in browser
 function showOutput(res) {
   document.getElementById('res').innerHTML = `
